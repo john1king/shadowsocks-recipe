50670f04d21a16c6dc343b04245f04679cf10785
john1king <uifantasy@gmail.com>
2015-03-11 17:02:58 +0800

    修改 cookbook 信息

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-name             'shadowsocks-recipe'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures shadowsocks-recipe'
+name             'shadowsocks'
+maintainer       'john1king'
+maintainer_email 'uifantasy@gmail.com'
+license          'MIT'
+description      'Install multiple shadowsocks servers'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 depends          'python'
